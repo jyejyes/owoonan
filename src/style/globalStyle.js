2a91618faecbe516dfe2269c2ea5367fd60759ba
brie <jh100m1@gmail.com>
2022-07-13 12:32:16 +0900

    :lipstick: style 파일 추가

---
+++ src/style/globalStyle.js
@@ -0,0 +1,20 @@
+//전역-스타일 모음
+import { createGlobalStyle } from "styled-components";
+import "./reset.css";
+
+export const GlobalStyle = createGlobalStyle`   
+    /* @font-face {
+        font-family: 'LeferiPoint-WhiteA';
+        src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2201-2@1.0/LeferiPoint-WhiteA.woff') format('woff');
+        font-weight: normal;
+        font-style: normal;
+    } */
+
+    html{
+        /* font-family: 'LeferiBaseType-RegularA'; */
+        font-size: 62.5%;
+        width:100%;
+        height:100%;
+    }
+    
+`;
