12a04955a5195139c1e172001b454da5a9af735e
Seongtaek Hwang <hst1993@hanmail.net>
2022-07-27 20:39:29 +0900

    💄 메뉴바 추가

--- src/components/exercise/exercise.tsx
+++ src/components/exercise/exercise.tsx
@@ -1,5 +1,14 @@
 import React from "react";
+import styled from "styled-components";
+import MenuBar from "../menuBar/menuBar";
 
-const Exercise = () => <h1>운동</h1>;
+const Exercise = () => (
+  <Container>
+    <h1>운동</h1>
+    <MenuBar />
+  </Container>
+);
 
 export default Exercise;
+
+const Container = styled.div``;
--- src/pages/RecordPage.tsx
+++ src/pages/RecordPage.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import MenuBar from "../components/menuBar/menuBar";
 
 const RecordPage = () => {
-  return <h1>기록</h1>;
+  return (
+    <>
+      <h1>기록</h1>
+      <MenuBar />
+    </>
+  );
 };
 
 export default RecordPage;
